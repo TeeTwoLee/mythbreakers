997afaa93f3c12e8fb3c6140a810ef095ff6418c
TeeTwoLee <teetwolee@gmail.com>
2021-07-02 14:20:40 -0400

    Deploy docs folder

--- docs/_next/static/i-_VtJTw7cf-_gPgz4qFF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],"/gura":["static/chunks/pages/gura-accdc61b19448ed6f1c9.js"],sortedPages:["/_app","/_error","/gura"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
